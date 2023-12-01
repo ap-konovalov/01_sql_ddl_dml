5090fd71d5ea349773eac14a5016aaf0cf27bbf2
APKonovalov <APKonovalov@alfabank.ru>
2023-12-01 21:04:32 +0300

    err

--- 01_create_customers.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE CUSTOMERS (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    surname VARCHAR(50),
-    age INT,
-    phone_number VARCHAR(50)
-)
--- 03_select.sql
+++
@@ -1,2 +0,0 @@
-SELECT product_name FROM ORDERS
-WHERE customer_id IN (SELECT id FROM CUSTOMERS WHERE name ILIKE 'alexey');
